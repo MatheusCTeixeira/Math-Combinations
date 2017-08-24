a605b5488f1d9b74930116a53d82a43de4da309f
Matheus Cândido Teixeira <matheuscteixeira@hotmail.com>
2017-08-24 14:01:00 -0500

    Update combinacao.cc

--- combinacao.cc
+++ combinacao.cc
@@ -2,43 +2,22 @@
 #include <string>
 #include <vector>
 
-int fat(int n)
-{
-    int result = 1;
-    while (n>1)    
-        result *= n--;       
-    return result;
-}
-
-int num(int n, int p)
-{
-    return static_cast<int>(fat(n)/(fat(p)*fat(n-p)));
-}
 
 template <typename T>
 void show(const std::vector<int>& v, const T& set)
 {
     for (int i=0;i<v.size(); ++i)
         std::cout << set[v[i]] << " ";
-    std::cout << std::endl;
+    std::cout << "\n";
 }
 
 template <typename T>
 void combinacao(const T& set, int n, int p)
-{
-    //Preconditions
-    if (n<p)
-        return;//throw?
-    ///////////
-
-    //Initialization
+{ 
     std::vector<int> vec;    
-
     for (int i=0; i<p ;++i)
-        vec.push_back(i);//First possible combination for all cases
-    ///////////////
-
-    //processamento    
+        vec.push_back(i);//primeira combinação possível para todos os casos  
+    
     while (true)
     {                
         show(vec, set);//exibe a combinação
@@ -53,16 +32,11 @@ void combinacao(const T& set, int n, int p)
         while (vec.size() < p)
             vec.push_back(vec.back() + 1);
     }
-    //////////////////
 }
 
 int main()
 {
-    std::vector<std::string> set{std::string{"A"},std::string{"B"},std::string{"C"},std::string{"D"},std::string{"E"}};    
-    combinacao<std::vector<std::string>>(set, set.size(), 3);
-    // or
-    //std::vector<std::string>
-    //combinacao<std::string>(set, set.size(), 3);
-
+    std::string set{"ABCDEFGHIJKLMNOPQRSTUVWXYZ"};    
+    combinacao<std::string>(set, set.size(), 6);
     return 0;
 }
